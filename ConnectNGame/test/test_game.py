7e408a8a56e8cb4dbe6f1b8ef07faf50443a07f3
Wendy Zhai <wyzhai@ucdavis.edu>
2020-05-07 11:41:49 -0700

    check_horizontal TestCases

--- ConnectNGame/test/test_game.py
+++ ConnectNGame/test/test_game.py
@@ -16,7 +16,26 @@ class TestGame(unittest.TestCase):
         ]
         self.assertTrue(Game.check_horizontal(3, 4, "o", 3, test_board_1))
 
+        test_board_2 = [
+            ["1", "1", "1", "1", "1"],
+            ["1", "x", "1", "1", "1"],
+            ["1", "1", "x", "x", "1"],
+            ["1", "1", "1", "1", "1"],
+            ["x", "x", "x", "x", "1"],
+            ["x", "x", "1", "x", "1"]
+        ]
+
+        self.assertTrue(Game.check_horizontal(6, 5, "x", 4, test_board_2))
+
     def test_check_horizontal_false(self):
+
+        test_board_1 = [
+            ["x", "x", "x", "x"],
+            ["x", "o", "o", "x"],
+            ["x", "x", "x", "x"]
+        ]
+        self.assertFalse(Game.check_horizontal(3, 4, "o", 3, test_board_1))
+
         test_board_2 = [
             ["1", "1", "1", "1", "1"],
             ["1", "x", "1", "1", "1"],
